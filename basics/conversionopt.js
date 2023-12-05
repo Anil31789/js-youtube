638ee3992932d5762f228b1aad377503b1b7fbc2
Anil Rai <98008532+Anil31789@users.noreply.github.com>
2023-12-05 08:16:52 +0000

    conversion of datatypes

---
+++ basics/conversionopt.js
@@ -0,0 +1,22 @@
+let score = "33abc";
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "anil"
+let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "anil" => true
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof(stringNum));
